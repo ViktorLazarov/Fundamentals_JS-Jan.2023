135e42cb087b861c27cd92e21b4fb96d6c6692fd
viktor lazarov <viktorlazarov@abv.bg>
2023-02-09 15:40:33 +0100

    finished 08

---
+++ Basic syntax,Conditional Statements and Loops-Exercise/08multiplicationTable.js
@@ -0,0 +1,11 @@
+function solve(number) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${number} X ${i} = ${number * i}`);
+    }
+}
+
+solve(10);
+console.log('**********************');
+solve(5);
+console.log('**********************');
+solve(2);

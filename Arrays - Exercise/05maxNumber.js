371130936bcb294948cffaf33e1dd4c712c62c5b
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-11 20:38:32 +0100

    Update 05maxNumber.js

--- Arrays - Exercise/05maxNumber.js
+++ Arrays - Exercise/05maxNumber.js
@@ -1,15 +1,30 @@
-function maxNumber(arr) {
-    let tempArr = [];
-    let biggerNumber = 0;
+function solve(arr) {
+    let resultArr = [];
+    let isBigger = true;
+
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > biggerNumber) {
-            biggerNumber = arr[i];
+        let current = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            if (current > arr[j]) {
+                isBigger = true;
+            } else {
+                isBigger = false;
+                break;
+            }
         }
-
+        if (isBigger) {
+            resultArr.push(arr[i]);
+        }
     }
+    if (arr[arr.length-2] <= arr[arr.length-1]) {
+        resultArr.push(arr[arr.length-1]);
+    }
+    console.log(resultArr.join(' '));
 
-    console.log(biggerNumber);
 }
 
-maxNumber([14, 24, 3, 19, 15, 17]);
-maxNumber([27, 19, 42, 2, 13, 45, 48]);
+solve([14, 24, 3, 19, 15, 17])
+solve([1, 4, 3, 2])
+solve([41, 41, 34, 20])
+solve([27, 19, 42, 2, 13, 45, 48])
+     

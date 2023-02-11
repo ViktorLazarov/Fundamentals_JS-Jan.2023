52a211e6e2f153e610df67d50ed0693d9d62950d
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-11 15:24:14 +0100

    Create 06evenAndOddSubtraction.js

---
+++ Arrays - Lab/06evenAndOddSubtraction.js
@@ -0,0 +1,17 @@
+function evenAndOddSubtraction(arr) {
+    let even = 0;
+    let odd = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Number(arr[i] % 2 === 0)) {
+            even += arr[i];
+        } else {
+            odd+= arr[i];
+        }
+    }
+    console.log(even-odd);
+}
+
+evenAndOddSubtraction([1,2,3,4,5,6])
+evenAndOddSubtraction([3,5,7,9])
+evenAndOddSubtraction([2,4,6,8,10])

c094215259ff1c220317c883741260b5d6cc3064
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-11 14:30:26 +0100

    Create 02dayOfWeek.js

---
+++ Arrays - Lab/02dayOfWeek.js
@@ -0,0 +1,16 @@
+function dayOfWeek(num) {
+    let day = ['Monday', 'Tuesday','Wednesday', 'Thursday','Friday','Saturday','Sunday'];
+
+    if (num < 1 || num > 7) {
+        console.log('Invalid Day!');
+    } else {
+      console.log(day[num-1]);
+    }
+
+}
+
+dayOfWeek(3);
+dayOfWeek(1);
+dayOfWeek(7);
+dayOfWeek(0);
+dayOfWeek(8);

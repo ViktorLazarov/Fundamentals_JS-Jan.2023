fbf29b5fab0b5d4d09828055d07b1949995e09bb
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-11 14:24:25 +0100

    renamed file and finished 01

---
+++ Arrays - Lab/01sumFirstAndLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumElements(arr) {
+    let result = arr[0] + arr[arr.length-1];
+    console.log(result);
+}
+
+sumElements([11, 58, 69])
+sumElements([10, 17, 22, 33])
+sumElements([20, 30, 40])

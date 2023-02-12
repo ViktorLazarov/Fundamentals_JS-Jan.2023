80efa8fe5e37f2275fda5d24d8523ebd0038574d
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-12 16:26:46 +0100

    Update 01formatGrade.js

--- Functions - Lab/01formatGrade.js
+++ Functions - Lab/01formatGrade.js
@@ -1,3 +1,18 @@
 function formatGrade(grade) {
-    
+    if (grade < 3) {
+        console.log(`Fail (2)`);
+    } else if (grade < 3.5){
+        console.log(`Poor (${grade.toFixed(2)})`);
+    } else if(grade <4.50){
+        console.log(`Good (${grade.toFixed(2)})`);
+    }else if(grade <5.50){
+        console.log(`Very Good (${grade.toFixed(2)})`);
+    }else {
+        console.log(`Excellent (${grade.toFixed(2)})`);
+    }
 }
+
+formatGrade(2.99)
+formatGrade(4.50)
+formatGrade(5.54)
+formatGrade(3.33)

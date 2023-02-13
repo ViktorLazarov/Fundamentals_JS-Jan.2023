58bb208ce7c559cd0c71d2d008dfd9242286c828
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-14 00:33:06 +0100

    Update 06passwordValidator.js

--- Functions - Exercises/06passwordValidator.js
+++ Functions - Exercises/06passwordValidator.js
@@ -6,83 +6,72 @@ function passwordValidator(password) {
     printMessage(validLength, validChars, validNumberOfNumbers);
 
     // Print Message for the User
-function printMessage(validLength, validChars, validCountOfNumbers) {
-    if (!validLength) {
-        console.log("Password must be between 6 and 10 characters");
-    }
-    if (!validChars) {
-        console.log("Password must consist only of letters and digits");
-    }
-    if (!validCountOfNumbers) {
-        console.log("Password must have at least 2 digits");
-    }
-    if (validLength && validChars && validCountOfNumbers) {
-        console.log("Password is valid");
-    }
-}
-
-
-// "Password must have at least 2 digits"
-function countNumberOfNumbers(password) {
-    let count = 0;
-    for (const char of password) {
-        let num = char.charCodeAt(0);
-        let isNumber = checkForNumbers(num);
-        if (isNumber) {
-            count++;
+    function printMessage(validLength, validChars, validCountOfNumbers) {
+        if (!validLength) {
+            console.log("Password must be between 6 and 10 characters");
+        }
+        if (!validChars) {
+            console.log("Password must consist only of letters and digits");
+        }
+        if (!validCountOfNumbers) {
+            console.log("Password must have at least 2 digits");
+        }
+        if (validLength && validChars && validCountOfNumbers) {
+            console.log("Password is valid");
         }
     }
 
-    if (count >= 2) {
-        return true
-    }
-    return false
-}
 
-// "Password must consist only of letters and digits"
-function validateChars(password) {
-    for (const char of password) {
-        let num = char.charCodeAt(0);
-        let isNumber = checkForNumbers(num);
-        let isLowerCase = checkForLowerCase(num);
-        let isUpperCase = checkForUpperCase(num);
-        if (!isNumber && !isLowerCase && !isUpperCase) {
-            return false;
+    // "Password must have at least 2 digits"
+    function countNumberOfNumbers(password) {
+        let count = 0;
+        for (const char of password) {
+            let num = char.charCodeAt(0);
+            let isNumber = checkForNumbers(num);
+            if (isNumber) {
+                count++;
+            }
         }
 
-    }
-    return true;
-}
-
-// "Password must be between 6 and 10 characters"
-function checkLength(password) {
-    if (password.length >= 6 && password.length <= 10) {
-        return true
-    } else {
+        if (count >= 2) {
+            return true
+        }
         return false
     }
-}
+
+    // "Password must consist only of letters and digits"
+    function validateChars(password) {
+        for (const char of password) {
+            let num = char.charCodeAt(0);
+            let isNumber = checkForNumbers(num);
+            let isLowerCase = checkForLowerCase(num);
+            let isUpperCase = checkForUpperCase(num);
+            if (!isNumber && !isLowerCase && !isUpperCase) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    // "Password must be between 6 and 10 characters"
+    function checkLength(password) {
+        return password.length >= 6 && password.length <= 10
+    }
 
 
-// Helper Functions 
-function checkForLowerCase(char) {
-    if (char >= 97 && char <= 122) {
-        return true
+    // Helper Functions 
+    function checkForLowerCase(char) {
+        return char >= 97 && char <= 122
     }
-    return false
-}
-function checkForUpperCase(char) {
-    if (char >= 65 && char <= 90) {
-        return true
+    function checkForUpperCase(char) {
+        return char >= 65 && char <= 90
+
     }
-    return false
-}
-function checkForNumbers(char) {
-    if (char >= 48 && char <= 57) {
-        return true
+    function checkForNumbers(char) {
+        return char >= 48 && char <= 57
+          
     }
-    return false
-}
 }
 
 

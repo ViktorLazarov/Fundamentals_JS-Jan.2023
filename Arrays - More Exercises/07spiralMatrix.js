5cab0e0808716dca4c6225a9ba39cc5e9d8b4b71
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-15 22:25:39 +0100

    Create 07spiralMatrix.js

---
+++ Arrays - More Exercises/07spiralMatrix.js
@@ -0,0 +1,15 @@
+function spiralMatrix(numA, numB) {
+    let result = [];
+    for (let i = 0; i < numA; i++) {
+        result.push(i)
+        for (let j = 1; j <= numB; j++) {
+            
+            
+        }
+        
+    }
+    console.log(result);
+
+}
+
+spiralMatrix(3,3)

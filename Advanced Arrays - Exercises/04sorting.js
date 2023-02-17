decdc39aa797824368c304367947ae55abfc218f
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-17 15:26:43 +0100

    Create 04sorting.js

---
+++ Advanced Arrays - Exercises/04sorting.js
@@ -0,0 +1,35 @@
+function sorting(inputArr) {
+    let result = [];
+    let iterations = inputArr.length;
+    inputArr.sort((a,b) => a - b);
+    // for (let i = 0; i < iterations; i++) {
+    //     if (i % 2 === 0) {
+    //         inputArr.sort((a, b)=> b - a)
+    //         result.push(inputArr.shift());
+    //         inputArr.sort((a, b)=> a - b)
+    //     } else {
+    //         // inputArr.sort((a, b)=> a - b)
+    //         result.push(inputArr.shift());
+    //         // inputArr.sort((a, b)=> a - b)
+    //     }
+        
+    // }
+    // for (let i = 0; i < iterations; i++) {
+    //     if (i % 2 === 0) {
+    //         result.push(inputArr.pop());
+    //     } else {
+    //         result.push(inputArr.shift());
+    //     }
+    // }
+    
+    while (inputArr.length != 0) {
+        result.push(inputArr.pop());
+        result.push(inputArr.shift());
+    }
+    console.log(result.join(' '));
+
+}
+
+
+sorting([1, 21, 3, 52, 69, 63, 31, 2, 18, 94]);
+sorting([34, 2, 32, 45, 690, 6, 32,7, 19, 47]);

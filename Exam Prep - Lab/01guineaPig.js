e5562424382f4ddd7270c684ddb7f967f0385554
viktor lazarov <viktorlazarov@abv.bg>
2023-02-18 09:22:27 +0100

    finished 01GuineaPig

--- Exam Prep - Lab/01guineaPig.js
+++ Exam Prep - Lab/01guineaPig.js
@@ -4,8 +4,23 @@ function main(inputArr) {
     let cover = Number(inputArr[2]) * 1000;
     let weigth = Number(inputArr[3]) * 1000;
 
-    console.log(food, hay, cover, weigth);
-    
+    for (let i = 1; i <= 30; i++) {
+        food -= 300;
+        if (i % 2 === 0) {
+            hay = hay - (food * 0.05);
+        }
+        if (i % 3 === 0) {
+            cover = cover - (weigth / 3)
+        }
+    }
+    if (food <= 0 || hay <= 0 || cover <= 0) {
+        console.log('Merry must go to the pet store!');
+    } else {
+        console.log(`Everything is fine! Puppy is happy! Food: ${(food/1000).toFixed(2)}, Hay: ${(hay/1000).toFixed(2)}, Cover: ${(cover/1000).toFixed(2)}.`);
+    }
+
 }
 
-main((["10","5","5.2","1"]))
+main((["10", "5", "5.2", "1"]))
+main((["9","5","5.2","1"]))
+main((["1","1.5","3","1.5"]))

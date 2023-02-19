c2d3bebcc2b1c1cadbb1499f80a99580aec5c07e
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-19 09:35:24 +0100

    Create 01computerStore.js

---
+++ Exam Prep 2/01computerStore.js
@@ -0,0 +1,41 @@
+function computerStore(inputArr) {
+    let sum = 0;
+    let command = inputArr.shift();
+
+    while (command !== 'special') {
+        if (command === 'regular') {
+            break;
+        }
+        let price = Number(command);
+        if (price < 0) {
+            console.log('Invalid Price!');
+            command = inputArr.shift();
+            continue;
+        }
+        sum += price;
+        command = inputArr.shift();
+    }
+    
+    if (sum === 0) {
+        console.log('Invalid Order!');
+    } else {
+        let taxes = sum * 0.2;
+        let totalPrice = sum * 1.2;
+        let discount = 0;
+        if (command === 'special') {
+            discount = totalPrice * 0.1;
+            totalPrice -= discount;
+        }
+        console.log("Congratulations you've just bought a new computer!");
+        console.log(`Price without taxes: ${sum.toFixed(2)}$`);
+        console.log(`Taxes: ${taxes.toFixed(2)}$`);
+        console.log('-----------');
+        console.log(`Total price: ${totalPrice.toFixed(2)}$`);
+    }
+
+
+}
+
+computerStore(['1050','200','450','2','18.50','16.86','special'])
+computerStore(['1023','15','-20','-5.50','450','20','17.66','19.30','regular'])
+computerStore(['regular'])

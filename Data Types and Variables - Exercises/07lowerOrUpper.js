dda1474fb4d9948ed8d198ffbd2d1e5eb52a15df
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-22 14:15:51 +0100

    Create 07lowerOrUpper.js

---
+++ Data Types and Variables - Exercises/07lowerOrUpper.js
@@ -0,0 +1,11 @@
+function lowerOrUpper(input) {
+    if(input.toUpperCase() === input){
+        console.log('upper-case');
+    } else {
+        console.log('lower-case');
+    }
+}
+
+
+lowerOrUpper('L')
+lowerOrUpper('n')

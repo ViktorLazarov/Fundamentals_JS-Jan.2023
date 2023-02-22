7924f21e9efa760df2423f7067120cf1f0c242f3
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-22 14:36:31 +0100

    Update 02addAndSubtract.js

--- Functions - Exercises/02addAndSubtract.js
+++ Functions - Exercises/02addAndSubtract.js
@@ -1,3 +1,17 @@
-function name(params) {
-    
+function addAndSubtract(num1, num2, num3) {
+    let sumResult = sum(num1, num2);
+    let subtractResult = subtract(sumResult, num3);
+
+    console.log((subtractResult));
+
+    function sum(a, b) {
+        return a + b;
+    }
+    function subtract(a, b) {
+        return a - b;
+    }
 }
+
+
+
+addAndSubtract(23, 6, 10)

8a2706010cf8b9541ddd58d8aa2521bb53d30b61
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-22 14:50:28 +0100

    Create 07NxNMatrix.js

---
+++ Functions - Exercises/07NxNMatrix.js
@@ -0,0 +1,14 @@
+function matrix(n) {
+    console.log();
+    for (let i = 0; i < n; i++) {
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(n)
+            
+        }
+        console.log(row.join(' '));
+        
+    }
+}
+
+matrix(3)

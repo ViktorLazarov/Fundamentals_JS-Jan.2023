f2477a6187a663a65cc367cafd26f7add292c4fe
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-25 17:37:41 +0100

    Update 04convertToJSON.js

--- Objects And Classes - Lab/04convertToJSON.js
+++ Objects And Classes - Lab/04convertToJSON.js
@@ -1,6 +1,10 @@
-function convertToJSON(name, lastName,hairColor) {
-    let person = {name: name, lastName: lastName, hairColor: hairColor}
+function convertToJSON(name, lastName, hairColor) {
+    let person = {
+        name: name,
+        lastName: lastName,
+        hairColor: hairColor
+    }
     console.log(JSON.stringify(person));
 }
 
-convertToJSON('George', 'Jones','Brown')
+convertToJSON('George', 'Jones', 'Brown')

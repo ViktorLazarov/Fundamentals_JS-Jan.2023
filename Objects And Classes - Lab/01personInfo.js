2ebc254d5e773212ccfa9191cd7e84ee4e4a3524
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-25 17:37:11 +0100

    Update 01personInfo.js

--- Objects And Classes - Lab/01personInfo.js
+++ Objects And Classes - Lab/01personInfo.js
@@ -1,6 +1,10 @@
 function personInfo(firstName, lastName, age) {
-    let person = {firstName: firstName, lastName: lastName, age: age};
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    };
     return person;
 }
 
-console.log(personInfo("Peter","Pan","20"));
+console.log(personInfo("Peter", "Pan", "20"));

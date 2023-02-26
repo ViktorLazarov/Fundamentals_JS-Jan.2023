77b412355211244b313f1eb346d5f7fd4dd40896
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-26 12:55:21 +0100

    Update 05cat.js

--- Objects And Classes - Lab/05cat.js
+++ Objects And Classes - Lab/05cat.js
@@ -1,3 +1,20 @@
-function name(params) {
-    
+function myCat(inputArr) {
+    class Cat {
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+        meow(){
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+    for (const cat of inputArr) {
+        let [name, age] = cat.split(' ');
+        let newCat = new Cat(name, age);
+        newCat.meow();
+    }
 }
+
+
+myCat(['Candy 1', 'Poppy 3', 'Nyx 2'])

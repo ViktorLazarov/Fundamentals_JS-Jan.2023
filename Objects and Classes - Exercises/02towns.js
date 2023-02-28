2c5835f7b813603143422c1bbe520a345171c946
Viktor Lazarov <viktorlazarov@abv.bg>
2023-02-28 18:26:10 +0100

    Update 02towns.js

--- Objects and Classes - Exercises/02towns.js
+++ Objects and Classes - Exercises/02towns.js
@@ -6,12 +6,19 @@ function towns(inputArr) {
             this.longitude = longitude;
         }
     }
-    
+
     for (let i = 0; i < inputArr.length; i++) {
         let townName = inputArr[i].split(' | ')[0];
         let latitude = Number(inputArr[i].split(' | ')[1]).toFixed(2);
         let longitude = Number(inputArr[i].split(' | ')[2]).toFixed(2);
         
+        // let town = {
+        //     town: townName,
+        //     latitude: latitude,
+        //     longitude: longitude,
+        // }
+        // console.log(town);
+
         let town = new Town(townName, latitude, longitude)
         console.log(JSON.parse(JSON.stringify(town)));
     }

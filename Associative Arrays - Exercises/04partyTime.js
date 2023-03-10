29b4ca9e51681797c5b345c268145212945579b7
Viktor Lazarov <viktorlazarov@abv.bg>
2023-03-10 15:54:38 +0100

    Update 04partyTime.js

--- Associative Arrays - Exercises/04partyTime.js
+++ Associative Arrays - Exercises/04partyTime.js
@@ -1,19 +1,19 @@
 function partyTime(data) {
-    let partyList = new Set();
+    let partyList = [];
     let command = data.shift();
 
     while (command !== 'PARTY') {
-        partyList.add(command)
+        partyList.push(command);
         command = data.shift()
     }
-    
+   
     for (const guest of data) {
-        if (partyList.has(guest)) {
-            partyList.delete(guest);
+        if (partyList.includes(guest)) {
+            partyList.splice(partyList.indexOf(guest),1);
         }
     }
     
-    let sorted = Array.from(partyList).sort();
+    let sorted = partyList.sort();
     console.log(sorted.length);
     for (const guest of sorted) {
         console.log(guest);
@@ -21,15 +21,53 @@ function partyTime(data) {
 
 }
 
+
+
 partyTime(['7IK9Yo0h',
-           '9NoBUajQ',
            'Ce8vwPmE',
+           '9NoBUajQ',
            'SVQXQCbc',
+           'tSzE5t0p',
+           'tSzE5t0p',
            'tSzE5t0p',
            'PARTY',
            '9NoBUajQ',
            'Ce8vwPmE',
-           'SVQXQCbc'])
+           'SVQXQCbc',
+           'tSzE5t0p'])
 
 console.log('******************');
-partyTime(['m8rfQBvl', 'fc1oZCE0','UgffRkOn', '7ugX7bm0', '9CQBGUeJ', '2FQZT3uC', 'dziNz78I', 'mdSGyQCJ', 'LjcVpmDL', 'fPXNHpm1', 'HTTbwRmM', 'B5yTkMQi', '8N0FThqG', 'xys2FYzn', 'MDzcM9ZK', 'PARTY', '2FQZT3uC', 'dziNz78I', 'mdSGyQCJ', 'LjcVpmDL', 'fPXNHpm1', 'HTTbwRmM', 'B5yTkMQi', '8N0FThqG', 'm8rfQBvl', 'fc1oZCE0', 'UgffRkOn', '7ugX7bm0', '9CQBGUeJ' ])
+partyTime([ 'm8rfQBvl',
+            'fc1oZCE0',
+            'UgffRkOn',
+            '7ugX7bm0',
+            '9CQBGUeJ',
+            '9CQBGUeJ',
+            '2FQZT3uC',
+            'dziNz78I',
+            'mdSGyQCJ',
+            'LjcVpmDL',
+            'LjcVpmDL',
+            'fPXNHpm1',
+            'HTTbwRmM',
+            'B5yTkMQi',
+            '8N0FThqG',
+            '8N0FThqG',
+            'xys2FYzn',
+            'MDzcM9ZK',
+            'PARTY',
+            '2FQZT3uC',
+            'dziNz78I',
+            'mdSGyQCJ',
+            'LjcVpmDL',
+            'LjcVpmDL',
+            'fPXNHpm1',
+            'HTTbwRmM',
+            'B5yTkMQi',
+            '8N0FThqG',
+            'm8rfQBvl',
+            'fc1oZCE0',
+            'UgffRkOn',
+            '7ugX7bm0',
+            '9CQBGUeJ' 
+        ])

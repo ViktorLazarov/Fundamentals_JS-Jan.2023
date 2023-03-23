f94181ccba745262ddb04a97a69e2aa6b2376f48
Viktor Lazarov <viktorlazarov@abv.bg>
2023-03-23 15:23:44 +0100

    Update 04starEnigma.js

--- Regular Expressions - Exercises/04starEnigma.js
+++ Regular Expressions - Exercises/04starEnigma.js
@@ -3,39 +3,51 @@ function starEnigma(input) {
     let starCountPattern = /[STARstar]/g;
     // let attackedPlanets = 0;
     // let destroyedPlanets = 0;
-    let attackedPlanetsList = [];
-    let destroyedPlanetsList = [];
-    let planetPattern = /(?<=@)(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)!(?<type>[A|D])!->(?<count>\d+)/;
+    let attackedPlanetsList = new Set();
+    let destroyedPlanetsList = new Set();
+    // let planetPattern = /(?<=@)(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)!(?<type>[A|D])!->(?<count>\d+)/;
     // let planetPattern = /(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)!(?<type>[AD])!->(?<count>\d+)/;
+    let planetPattern = /[^@\-!:>\s]*@(?<name>[A-Za-z]+)[^@\-!:>]*:(?<population>\d+)[^@\-!:>\s]*!(?<type>[A|D])![^@\-!:>\s]*\->(?<count>\d+)/;
     
     
     for (const line of input) {
-        let starCount = line.match(starCountPattern).length;
+        let starCount = 0;
+        let match = starCountPattern.exec(line);
+        
+        while (match !== null) {
+            starCount ++;
+            match = starCountPattern.exec(line);
+        }
+        
+
         let newMessage = '';
         for (let i = 0; i < line.length; i++) {
             let newChar = String.fromCharCode(line.charCodeAt(i)-starCount);
             newMessage += newChar;
         }
+        
         if (planetPattern.test(newMessage)) {
             let match = newMessage.match(planetPattern);
+
             if (match.groups.type === 'A') {
                 // attackedPlanets ++;
-                attackedPlanetsList.push(match.groups.name)
+                attackedPlanetsList.add(match.groups.name)
             } else if(match.groups.type === 'D'){
                 // destroyedPlanets ++;
-                destroyedPlanetsList.push(match.groups.name)
+                destroyedPlanetsList.add(match.groups.name)
             }
         }
     }
-    console.log(`Attacked planets: ${attackedPlanetsList.length}`);
-    if (attackedPlanetsList.length) {
-        for (const planet of attackedPlanetsList.sort((a,b) => a.localeCompare(b))) {
+    console.log(`Attacked planets: ${attackedPlanetsList.size}`);
+    
+    if (attackedPlanetsList.size) {
+        for (const planet of Array.from(attackedPlanetsList).sort((a,b) => a.localeCompare(b))) {
             console.log(`-> ${planet}`);
         }
     }
-    console.log(`Destroyed planets: ${destroyedPlanetsList.length}`);
-    if (destroyedPlanetsList.length) {
-        for (const planet of destroyedPlanetsList.sort((a,b) => a.localeCompare(b))) {
+    console.log(`Destroyed planets: ${destroyedPlanetsList.size}`);
+    if (destroyedPlanetsList.size) {
+        for (const planet of Array.from(destroyedPlanetsList).sort((a,b) => a.localeCompare(b))) {
             console.log(`-> ${planet}`);
         }
     }

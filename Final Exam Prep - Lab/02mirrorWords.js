d29c95b4fb3a4a60028daa4a2331ff335c1bd426
viktor lazarov <viktorlazarov@abv.bg>
2023-03-25 17:45:03 +0100

    created 02mirrorWords.js

--- Final Exam Prep - Lab/02mirrorWords.js
+++ Final Exam Prep - Lab/02mirrorWords.js
@@ -1,14 +1,35 @@
-function mirrorWords(input) {
+function mirrorWords(arr) {
+    let input = arr.shift();
     let pattern = /([@#])(?<leftWord>[A-Za-z]{3,})\1\1(?<rightWord>[A-Za-z]{3,})\1/g;
-    let matches = [...input.matchAll(pattern)];
-    
+    let matches = Array.from(input.matchAll(pattern));
+    let pairCount = matches.length;
+    let mirrorWordsCount = 0;
+    let mirrorWordsList = [];
     for (const match of matches) {
-        console.log(match.groups.leftWord);
-        console.log(match.groups.rightWord);
+        let leftWord = match.groups.leftWord;
+        let rightWord = match.groups.rightWord;
+        let reversedRightWord = rightWord.split('').reverse().join('');
+        if (leftWord === reversedRightWord) {
+            mirrorWordsCount++;
+            mirrorWordsList.push(`${leftWord} <=> ${rightWord}`)
+        }
 
+
+    }
+
+    if (!pairCount) {
+        console.log(`No word pairs found!`);
+        console.log(`No mirror words! `);
+    } else {
+        console.log(`${pairCount} word pairs found!`);
+        if (!mirrorWordsCount) {
+            console.log(`No mirror words! `);
+        }else {
+            console.log(`The mirror words are:\n${mirrorWordsList.join(', ')}`);
+        }
     }
 
     
 }
 
-mirrorWords('@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r')
+mirrorWords(['@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r'])

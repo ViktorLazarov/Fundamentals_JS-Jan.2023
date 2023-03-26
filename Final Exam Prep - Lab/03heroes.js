226fbe590418ca7e3ef02efadf9ec82077344e48
viktor lazarov <viktorlazarov@abv.bg>
2023-03-26 11:38:02 +0200

    finished 03heroes.js

--- Final Exam Prep - Lab/03heroes.js
+++ Final Exam Prep - Lab/03heroes.js
@@ -1,6 +1,103 @@
 function heroes(inputArr) {
-    let heroesCount = inputArr.shift();
-    
+    let heroesCount = Number(inputArr.shift());
+    let heroesList = new Map();
+    class Hero {
+        constructor(name, hp, mana) {
+            this.name = name;
+            this.hp = hp;
+            this.mana = mana;
+        }
+    }
+
+
+
+    for (let i = 0; i < heroesCount; i++) {
+        let tokens = inputArr.shift().split(' ');
+        let name = tokens[0];
+        let values = tokens.slice(1);
+        let hp = Number(values[0]);
+        let mana = Number(values[1]);
+        let hero = new Hero(name, hp, mana);
+        heroesList.set(name, hero);
+    }
+    for (const line of inputArr) {
+        let tokens = line.split(' - ');
+        let command = tokens[0];
+        let values = tokens.slice(1);
+        switch (command) {
+            case 'Heal': {
+                let [heroName, amount] = values;
+                heal(heroName, Number(amount), heroesList);
+                break;
+            }
+            case 'Recharge': {
+                let [heroName, amount] = values;
+                recharge(heroName, Number(amount), heroesList);
+                break;
+            }
+            case 'CastSpell': {
+                let [heroName, mpNeeded, spellName] = values;
+                let heroMana = heroesList.get(heroName).mana;
+                castSpell(heroName, heroMana, Number(mpNeeded), spellName, heroesList);
+                break;
+            }
+            case 'TakeDamage': {
+                let [heroName, damage, attacker] = values;
+                takeDamage(heroName, Number(damage), attacker, heroesList);
+                break;
+            }
+            case 'End': {
+                end(heroesList);
+                return;
+            }
+
+
+
+
+        }
+    }
+
+
+    function castSpell(heroName, heroMana, mpNeeded, spellName, heroMap) {
+        if (heroMana >= mpNeeded) {
+            heroMana = heroMana - mpNeeded;
+            heroMap.get(heroName).mana = heroMana;
+            console.log(`${heroName} has successfully cast ${spellName} and now has ${heroMana} MP!`);
+        } else {
+            console.log(`${heroName} does not have enough MP to cast ${spellName}!`);
+        }
+    }
+    function takeDamage(heroName, damage, attacker, heroesList) {
+        let currentHP = heroesList.get(heroName).hp;
+        if (currentHP - damage > 0) {
+            currentHP -= damage;
+            heroesList.get(heroName).hp = currentHP;
+            console.log(`${heroName} was hit for ${damage} HP by ${attacker} and now has ${currentHP} HP left!`);
+        } else {
+            heroesList.delete(heroName);
+            console.log(`${heroName} has been killed by ${attacker}!`);
+        }
+    }
+    function heal(heroName, amount, heroesMap) {
+        let healedAmount = Math.min(100 - heroesMap.get(heroName).hp, amount);
+        heroesMap.get(heroName).hp += healedAmount;
+        console.log(`${heroName} healed for ${healedAmount} HP!`);
+    }
+    function recharge(heroName, amount, heroesMap) {
+        let recharedAmount = Math.min(200 - heroesMap.get(heroName).mana, amount);
+        heroesMap.get(heroName).mana += recharedAmount;
+        console.log(`${heroName} recharged for ${recharedAmount} MP!`);
+    }
+    function end(heroesList) {
+        let heroesArr = heroesList.values();
+        
+        for (const hero of heroesArr) {
+            let name = hero.name;
+            let hp = hero.hp;
+            let mp = hero.mana;
+            console.log(`${name}\n  HP: ${hp}\n  MP: ${mp}`);
+        }
+    }
 
 
 }
@@ -13,3 +110,16 @@ heroes(['2',
     'TakeDamage - Kyrre - 66 - Orc',
     'CastSpell - Kyrre - 15 - ViewEarth',
     'End'])
+
+    console.log('****************');
+heroes(['4',
+'Adela 90 150',
+'SirMullich 70 40',
+'Ivor 1 111',
+'Tyris 94 61',
+'Heal - SirMullich - 50',
+'Recharge - Adela - 100',
+'CastSpell - Tyris - 1000 - Fireball',
+'TakeDamage - Tyris - 99 - Fireball',
+'TakeDamage - Ivor - 3 - Mosquito',
+'End'])

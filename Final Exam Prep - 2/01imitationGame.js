a96ec736c54aa4b753910393d2c69b2c6860c5fd
viktor lazarov <viktorlazarov@abv.bg>
2023-03-27 09:09:32 +0200

    sdsdfsdf

--- Final Exam Prep - 2/01imitationGame.js
+++ Final Exam Prep - 2/01imitationGame.js
@@ -6,23 +6,32 @@ function imitationGame(input) {
         let values = tokens.slice(1);
         switch (command) {
             case 'ChangeAll': {
-                message = message.split(values[0]).join(values[1]);
+                // message = message.split(values[0]).join(values[1]);
+                let removeLetter = values[0];
+                let insertLetter = values[1];
+                while (message.includes(removeLetter)) {
+                    message = message.replace(removeLetter, insertLetter)
+                }
                 break;
             }
             case 'Insert': {
-                message = message.split('');
-                message.splice(Number(values[0]),0,values[1]);
-                message = message.join('');
+                // message = message.split('');
+                // message.splice(Number(values[0]),0,values[1]);
+                // message = message.join('');
+                let n = Number(values[0]);
+                message = message.substring(0,n) + values[1] + message.substring(n);
                 break;
             }
             case 'Move':{
                 let moveCount = values[0];
-                message = message.split('');
+                // message = message.split('');
                 for (let i = 0; i < moveCount; i++) {
-                    let temp = message.shift();
-                    message.push(temp)                   
+                    let firstLetter = message.substring(0,1);
+                    let restLetters = message.substring(1);
+                    message = restLetters + firstLetter;
+                    // message.push(temp)                   
                 }
-                message = message.join('');
+                // message = message.join('');
                 break;
             }
             case 'Decode': {

20e97a0d37a6a1d2e3ccb5e881a3f490a2010c89
Viktor Lazarov <viktorlazarov@abv.bg>
2023-03-30 11:14:26 +0200

    Create 01revealWord.js

---
+++ Text Processing - Exercises/01revealWord.js
@@ -0,0 +1,19 @@
+function revealWord(input1, input2) {
+    let inputWords = input1.split(', ');
+    let sentenceWords = input2.split(' ');
+    
+    for (let i = 0; i < inputWords.length; i++) {
+        for (let j = 0; j < sentenceWords.length; j++) {
+            let inputWord = inputWords[i];
+            let replacedWord = sentenceWords[j];
+            debugger
+            if (inputWord.length === replacedWord.length && replacedWord.includes('*')) {
+                sentenceWords[j] = inputWord;
+            }
+        }
+    }
+    console.log(sentenceWords.join(' '));
+    
+}
+
+revealWord('great, learning', 'softuni is ***** place for ******** new programming languages')

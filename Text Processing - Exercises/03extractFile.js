3075747cdf4a8753468be13c92c715b5d067ff91
Viktor Lazarov <viktorlazarov@abv.bg>
2023-04-01 14:19:26 +0200

    Create 03extractFile.js

---
+++ Text Processing - Exercises/03extractFile.js
@@ -0,0 +1,12 @@
+function extractFile(input) {
+    let lastIndex = input.lastIndexOf('\\');
+    let file = input.substring(lastIndex+1);
+    let dotIndex = file.lastIndexOf('.');
+    let fileName = file.substring(0,dotIndex);
+    let fileExtension = file.substring(dotIndex+1)
+    console.log(`File name: ${fileName}`);
+    console.log(`File extension: ${fileExtension}`);
+    
+}
+
+extractFile('C:\\Internal\\training-internal\\Template.pptx')
